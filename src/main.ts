1615bf4a1874c01ddf24b34aa3e0e3d12bab6475
Jason D'Souza <jasondsouza2003@gmail.com>
2025-07-03 09:41:28 +0800

    V0.1.1 - Graphics Module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-#!/usr/bin/env -S deno run -A
+#!/usr/bin/env -S deno run -A --watch
 /** # LasorTech POS System
   * @module
   */
-import { xml } from './xml.ts';
 import { Program } from './program.ts';
 
 
-const _window = await new Program(xml`
-    <h1>Hello World</h1>
-`).init();
+const app = new Program();
+await app.gui();
 
